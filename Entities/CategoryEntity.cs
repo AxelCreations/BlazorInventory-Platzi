9a5a3e73b8345074f0e80739f38f1d69a603b0c5
AxelCreations <reynelaxel@gmail.com>
2020-10-30 11:20:24 -0400

    Added inventory entities

---
+++ Entities/CategoryEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Entities
+{
+    public class CategoryEntity
+    {
+        [Key]
+        [StringLength(10)]
+        public string CategoryID { get; set; }
+        
+        [Required]
+        [StringLength(100)]
+        public string CategoryName { get; set; }
+
+        public ICollection<ProductEntity> Products { get; set; }
+    }
+}
